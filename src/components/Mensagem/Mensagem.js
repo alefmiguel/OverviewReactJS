d2e1230c5b4bddcee770c29a5387815f4b146632
alefmiguel <alefmiguel45@gmail.com>
2023-04-05 16:51:11 -0300

    enfim, continuando o estudo sobre react no curso do Mateus do ytb

---
+++ src/components/Mensagem/Mensagem.js
@@ -0,0 +1,11 @@
+import styles from "./Mensagem.modules.css";
+
+function Mensagem() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.titulo}> Olá, meu nome é Álef! </h1>
+    </div>
+  );
+}
+
+export default Mensagem;
